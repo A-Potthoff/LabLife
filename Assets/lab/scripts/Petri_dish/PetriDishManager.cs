c444468fc6cfff4c0d1ff2f5c85afd07a652c8ec
Andre <andre.potthoff@yahoo.com>
2024-06-27 12:20:44 +0200

    modularization of start_minigame and object specific scripts
    object2scene can now be used for every object and then there will be a second _Manager script that contains all the attributes and a cusom Start_minigame function (to open different scenes etc.)

---
+++ Assets/lab/scripts/Petri_dish/PetriDishManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_specific_manager : MonoBehaviour
+{
+    public void Start_minigame()
+    {
+        // Define the logic for starting the minigame here
+        Debug.Log("Minigame started!");
+    }
+}
--- Assets/lab/scripts/Petri_dish/object2scene.cs
+++ Assets/lab/scripts/Petri_dish/object2scene.cs
@@ -5,11 +5,22 @@ public class InteractiveObject : MonoBehaviour
     private Vector3 originalScale;
     private Vector3 enlargedScale;
     private bool isPlayerInContact = false;
+    [SerializeField] private Object_specific_manager object_specific_manager;
+
+    [Header("Petri dish attributes")] //to make the unity scene more readable
+
+    [SerializeField] public bool is_empty;
+    [SerializeField] public string contains;
+    [SerializeField] public bool culture_has_grown = false;
+    [SerializeField] public bool coverage;
 
     private void Start()
     {
         originalScale = transform.localScale;
         enlargedScale = originalScale * 1.1f; // Increase scale by 10%
+
+        //get component of the object specific manager
+        object_specific_manager = GetComponent<Object_specific_manager>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -35,13 +46,7 @@ public class InteractiveObject : MonoBehaviour
         Debug.Log("Object clicked");
         if (isPlayerInContact)
         {
-            start_minigame();
+            object_specific_manager.Start_minigame();
         }
-    }
-
-    private void start_minigame()
-    {
-        // Define the logic for starting the minigame here
-        Debug.Log("Minigame started!");
     }
 }
