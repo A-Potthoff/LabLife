e157427335b821e516ac98c2c48e8d3bb0d02354
Andre <andre.potthoff@yahoo.com>
2024-06-30 21:59:13 +0200

    Feierabend für heute
    unfinished steps

--- Assets/centrifugation_scene/scripts/WeightController.cs
+++ Assets/centrifugation_scene/scripts/WeightController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class WeightController : MonoBehaviour
 {
-    private Vector3 offset;
+    /*private Vector3 offset;
+    private Transform currentHolder = null;
+
+    //----------------------------- mouse movement ------------------------------------
 
     void OnMouseDown() //necessary to calculate the offset
     {
@@ -27,4 +30,81 @@ public class WeightController : MonoBehaviour
         // Apply the offset to keep the pipette from jumping to the mouse position
         transform.position = worldPosition + offset;
     }
+
+    //----------------------------- interaction with centrifuge ------------------------
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Holder"))
+        {
+            currentHolder = other.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Holder"))
+        {
+            if (currentHolder == other.transform)
+            {
+                currentHolder = null;
+            }
+        }
+    }
+    */
+
+    private bool isDragging = false;
+    private Vector3 offset;
+    private Transform currentHolder = null;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (GetComponent<Collider2D>().OverlapPoint(mousePos))
+            {
+                isDragging = true;
+                offset = transform.position - mousePos;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0) && isDragging)
+        {
+            isDragging = false;
+            if (currentHolder != null)
+            {
+                // Snap to the holder
+                transform.position = currentHolder.position;
+                transform.SetParent(currentHolder);
+            }
+        }
+
+        if (isDragging)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+            transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Holder"))
+        {
+            currentHolder = other.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Holder"))
+        {
+            if (currentHolder == other.transform)
+            {
+                currentHolder = null;
+            }
+        }
+    }
+
+
 }
