4bdc09529d3a24068f0bab24b178cd081676e9e2
Andre <andre.potthoff@yahoo.com>
2024-07-02 00:18:14 +0200

    somewhat fixed an error
    still to be resolved: if a tube gets correctlly rejected from a filled tube, once the player wants take the tube away the holder then gets set to false.

--- Assets/centrifugation_scene/scripts/WeightController.cs
+++ Assets/centrifugation_scene/scripts/WeightController.cs
@@ -4,7 +4,9 @@ public class WeightController : MonoBehaviour
 {
     private Vector3 offset;
     [SerializeField] private Transform CurrentHolder = null;
-    private Holder HolderScript = null;
+    private Holder CurrentHolderScript = null;
+    private Holder PreviousHolderScript = null;
+    private Collider2D holderCollider;
 
     private SpriteRenderer spriteRenderer;
 
@@ -17,6 +19,12 @@ public class WeightController : MonoBehaviour
 
     void OnMouseDown() //necessary to calculate the offset
     {
+        // Mark the current holder as empty when the tube is picked up
+        if (CurrentHolderScript != null)
+        {
+            CurrentHolderScript.filled = false;
+        }
+
         // Convert mouse position to world position
         Vector3 mousePosition = Input.mousePosition;
         mousePosition.z = Camera.main.WorldToScreenPoint(transform.position).z;
@@ -24,38 +32,46 @@ public class WeightController : MonoBehaviour
 
         // Calculate the offset between the mouse position and the pipette's position
         offset = transform.position - worldPosition;
+
+        // Track the previous holder
+        PreviousHolderScript = CurrentHolderScript;
     }
 
     void OnMouseUp()
     {
-        if (HolderScript != null) // if the tube was previously in a holder
-        {
-            HolderScript.filled = false; // set the previous holder to empty
-        }
-        
         // Check if mouse is over a holder collider
-        Collider2D holderCollider = GetHolderUnderMouse();
-        HolderScript = holderCollider?.GetComponent<Holder>(); // Get the Holder script component of the holder collider
-        if (holderCollider != null && !HolderScript.filled)
+        holderCollider = GetHolderUnderMouse();
+        CurrentHolderScript = holderCollider?.GetComponent<Holder>(); // Get the Holder script component of the holder collider
+
+        if (holderCollider != null && !CurrentHolderScript.filled)
         {
             // Snap to the holder's position
             CurrentHolder = holderCollider.transform;
             transform.position = CurrentHolder.position;
 
             Vector3 holderEulerAngles = CurrentHolder.rotation.eulerAngles;    // Get the current holder's rotation
-            transform.rotation = Quaternion.Euler(holderEulerAngles.x, holderEulerAngles.y, 180f + holderEulerAngles.z); // Set the rotation to 180 - holder's z rotation
+            transform.rotation = Quaternion.Euler(holderEulerAngles.x, holderEulerAngles.y, 180f + holderEulerAngles.z); // Set the rotation to 180 + holder's z rotation
 
             transform.SetParent(CurrentHolder);
             Debug.Log("Weight Snapped to holder: " + CurrentHolder.name);
 
-            // set the holder to filled
-            HolderScript.filled = true;
+            // Set the current holder to filled
+            CurrentHolderScript.filled = true;
+
+            // Clear the previous holder reference
+            PreviousHolderScript = null;
         }
         else
         {
             transform.SetParent(null);
             CurrentHolder = null;
             transform.rotation = Quaternion.identity;
+
+            // If there was a previous holder, make sure to mark it as empty
+            if (PreviousHolderScript != null)
+            {
+                PreviousHolderScript.filled = false;
+            }
         }
     }
 
